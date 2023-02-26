ca58930ee96465d164e4ac510f01076e885878b5
hasancanmidi <hasancanmidi@hotmail.com>
2023-02-26 06:31:02 +0300

    I added modulo operator

---
+++ 4.Modulo Operator/moduloOperator.js
@@ -0,0 +1,21 @@
+//Addition - Toplama
+var a = 2 + 4;
+
+//Subraction - Çıkarma
+var b = 10 - 2;
+
+//Multiplacation - Çarpma
+var c = 5 * 5;
+
+//Division - Bölme 
+var d = 20 / 5;
+
+// Modulo - bölümden kalanı verir
+var e = 9 % 6; // 3
+
+// Increment - Artış ifadesi
+x++; // x = x + 1 anlamına gelir
+x+=2 // birden fazla artırmak istersek
+
+// Decrement - Azaltma ifadesi
+x--; // x = x - 1 anlamına gelir
