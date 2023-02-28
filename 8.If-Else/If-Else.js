4446da8ba2d99dcdd62d7bd81b60afbd45e48e68
hasancanmidi <hasancanmidi@hotmail.com>
2023-02-28 21:53:24 +0300

    Update

---
+++ 8.If-Else/If-Else.js
@@ -0,0 +1,17 @@
+// typeof(a);
+// === is equal to   - Sol tarafın sağ tarafa eşit olup olmadığını kontrol eder.  ayrıca 3 eşittir tipinide sorgular (number,string...)
+// !== is not equal to   - Sol tarafın sağ tarafa eşit olup olmadığını kontrol eder.
+// > is greater than   
+// > is lesser than   
+// >= is greater or equal than   
+// <= is lesser or equal than   
+// && AND - ve iki koşulunda ikisi dogru olmalı
+// || OR - veya iki koşuldan birisi dogruysa
+// ! NOT - değil
+
+if (track === "clear"){
+    goStraight();
+} else {
+    turnRight();
+}
+
