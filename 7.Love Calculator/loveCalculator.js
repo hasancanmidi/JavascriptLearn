db4cbf3bc9248a975f10026a53c9c881a8840ed2
hasancanmidi <hasancanmidi@hotmail.com>
2023-02-28 10:05:52 +0300

    Update love calculator

--- 7.Love Calculator/loveCalculator.js
+++ 7.Love Calculator/loveCalculator.js
@@ -5,5 +5,11 @@ prompt("What is your name");
 prompt("What is their name");
 
 var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1; //1-100
 
-console.log("Your love score " + Math.round(loveScore));
+if(loveScore === 100){
+    alert("Your love score is " + loveScore + " %" + " You love each other like Kanye loves Kanye.")
+}
+else{
+    alert("Your love score is " + loveScore + " %" + " ")
+}
