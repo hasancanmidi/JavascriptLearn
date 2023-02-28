487ee10f4f12fd9ad6f17d26394a573f626be919
hasancanmidi <hasancanmidi@hotmail.com>
2023-02-28 09:48:51 +0300

    love calculator

---
+++ 7.Love Calculator/loveCalculator.js
@@ -0,0 +1,9 @@
+// Random Number Generation
+// var n = math.random();
+
+prompt("What is your name");
+prompt("What is their name");
+
+var loveScore = Math.random() * 100;
+
+console.log("Your love score " + Math.round(loveScore));
