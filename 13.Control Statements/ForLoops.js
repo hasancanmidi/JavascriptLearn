731f40a0f105999408c39bc54012c39cb8027a3a
hasancanmidi <hasancanmidi@hotmail.com>
2023-03-02 06:43:42 +0300

    Create for loops

---
+++ 13.Control Statements/ForLoops.js
@@ -0,0 +1,28 @@
+//For Loops
+
+/*
+for (i=0; i<10; i++){
+    console.log("Hasan Can Midi " + i);
+}
+*/
+
+//////////////////////////////////////////
+var eggs = [];
+
+function fizzbuzz() {
+    for (var count = 1; count <= 100; count++) {
+        if (count % 3 === 0 && count % 5 === 0) {
+            eggs.push("fizzbuzz");
+        }
+        else if (count % 3 === 0) {
+            eggs.push("fizz");
+        }
+        else if (count % 5 === 0) {
+            eggs.push("buzz");
+        }
+        else {
+            eggs.push(count);
+        }
+    }
+    console.log(eggs);
+}
