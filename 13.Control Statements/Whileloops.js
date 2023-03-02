a0b9bf4d24323b53fbcb2815f40f514277e751d4
hasancanmidi <hasancanmidi@hotmail.com>
2023-03-02 03:45:25 +0300

    update

--- 13.Control Statements/Whileloops.js
+++ 13.Control Statements/Whileloops.js
@@ -33,24 +33,24 @@ function fizzbuzz() {
 
 
 /////////////////////////////////////////////
-beer(){
-    var numberOfBottles = 99
 
-    while (numberOfBottles >= 0) {
-        var bottleWord = "bottle";
+var numberOfBottles = 99
 
-        if (numberOfBottles === 1) {
-            bottleWord = "bottles";
-        }
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
 
-        console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
-
-        console.log(numberOfBottles + " " + bottleWord + " of beer,");
-
-        console.log("Take one down, pass it around,");
-
-        numberOfBottles--;
-
-        console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
     }
+
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+
+    console.log("Take one down, pass it around,");
+
+    numberOfBottles--;
+
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 }
+
