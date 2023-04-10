fc805075afdffb3be729eaa2a79ca0ebc17a9d19
hasancanmidi <hasancanmidi@hotmail.com>
2023-04-10 11:09:45 +0300

    Update data types

--- 2.Data Types/data-types.js
+++ 2.Data Types/data-types.js
@@ -1,5 +1,24 @@
 //const, let, var
 
+let name = "Hasan Can";
+const age = 24; // const soradan değiştirilemez. bir veri tipidir.
+const number1 = "10"; // bu bir stringdir
+let number2 = 10.6666666;
+const result = false;
+
+// String, Number, Boolean
+
+name = "deneme deneme";
+
+console.log(Number(number1) + age); //Number(number1) tipini değiştim.
+console.log(number1 + age.toString); //age.toString() ifadeyi string bir ifadeye çevirir
+
+// NaN = Tanımsız.
+// Undefined = Tanımsız.
+// Null = boş.
+
+console.log(number2.toFixed(2)); // toFixed ile ondalıklı sayının virgülden sonra istediğimiz kadarını almamıza yarayan kod.
+
 // Sayısal veri tipleri
 
 let x = 5;       // tam sayı (integer)
